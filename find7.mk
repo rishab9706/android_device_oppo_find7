78a58bee6db7ff697ddce98839de8fb36c732805
Rishab Nair <rishabnair2001@gmail.com>
2019-03-09 06:54:56 +0000

    find7: Aex specific cleanup

--- find7.mk
+++ find7.mk
@@ -15,7 +15,9 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aex \
 
 # Ramdisk
 PRODUCT_PACKAGES += \
